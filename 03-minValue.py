8f9a47eac35403461b0314c9f892bcfbcef4cea6
oilyn0s3 <oilyn0s3@gmail.com>
2022-09-02 19:00:11 +0530

    03minValue done

---
+++ 03-minValue.py
@@ -0,0 +1,13 @@
+from platform import java_ver
+
+
+def min_value(array):
+    min_index = 0
+    for i in range(1,len(array)-1):
+        if array[min_index] > array[i]:
+            min_index = i
+            i += 1
+        else:
+            i += 1
+    print(f'min value: {array[min_index]}')
+min_value([10,9,4,3,7])
